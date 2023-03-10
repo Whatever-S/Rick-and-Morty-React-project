c2c5840c1357af6be47299fa75583a27a5885cba
Sasha <sashla1713@gmail.com>
2023-03-10 17:46:49 +0200

    added characters list rendering

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchPanel from "./components/search-panel/Search-panel";
 
 import './App.scss'
 import { useEffect, useState } from "react";
+import CharList from "./components/char-list/Char-list";
 
 
 function App() {
@@ -10,20 +11,19 @@ function App() {
   let _apiBase = `https://rickandmortyapi.com/api/character`;
 
   let [receivedData, updateReceivedData] = useState([]);
-  console.log(receivedData.results)
-    
 
-    useEffect(()=>{
-      (async function(){
-        let data = await fetch(_apiBase).then(res => res.json())
-        updateReceivedData(data)
-      })();
-    },[_apiBase]);
+  useEffect(()=>{
+    (async function(){
+      let data = await fetch(_apiBase).then(res => res.json())
+      updateReceivedData(data)
+    })();
+  },[_apiBase]);
 
   return (
     <div className="App container">
       <Title/>
       <SearchPanel/>
+      <CharList characters={receivedData.results}/>
     </div>
   );
 }
