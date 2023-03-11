01c2fc8c42594ba666100d43a1e23e0bbe9cef76
Oleksandra <sashla1713@gmail.com>
2023-03-11 13:34:39 +0200

    Char list desing fix

--- src/components/char-list/Char-list.js
+++ src/components/char-list/Char-list.js
@@ -2,31 +2,34 @@ import './char-list.scss'
 
 function CharList({characters}){
    //console.log(characters)
-   let content
+    let content,
+        counter = 0
 
    if(characters){
-    
         content = characters.map(char => {
+            console.log(char)
+            while(counter < 8) {
             const {name,id, species, image} = char
-            
+            counter++;
+
             return (
             <div key={id} className="singlecharacter">
-                <img src={image} alt='Character image'className="singlecharacter__img"/>
+                <img src={image} alt='Character img' className="singlecharacter__img"/>
                 <div className="singlecharacter__info">
                     <p className="singlecharacter__name">{name}</p>
                     <p className="singlecharacter__specie">{species}</p>
                 </div>
             </div>
-            )
+            )}
     })
-    
+
    }
    else{
     content = "There is no character with such name..."
    }
     return (
-    <div className='list__container'>
-        <div>{content}</div>
+    <div className='list__wrapper'>
+        {content}
     </div>
    )
 }
